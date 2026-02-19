17f8e37133ebf44d64c88d5c7f944347ae95c4be
Quinn Trusler <quinn@trusler.net>
2026-02-19 14:56:57 -0500

    Add functions to manage mana bar

--- Quinn/health_layer.gd
+++ Quinn/health_layer.gd
@@ -8,7 +8,13 @@ func set_boss_max_health(max_health):
 	$EnemyHealthBar.max_value = max_health
 	$EnemyHealthBar.value = $EnemyHealthBar.max_value
 
+func set_player_max_mana(max_mana):
+	$PlayerManaBar.max_value = max_mana
+	$PlayerManaBar.value = $PlayerMAnaBar.max_value
+
 func update_player_health_bar(value: float):
 	$PlayerHealthBar.value = value
 func update_boss_health_bar(value:float):
 	$EnemyHealthBar.value = value
+func update_player_mana_bar(value:float):
+	$PlayerManaBar.value = value
