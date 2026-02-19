18113040b33e68361f2ac0c4471124cae9c57393
Okwara-Favour <65445852+Okwara-Favour@users.noreply.github.com>
2026-02-19 00:25:55 -0800

    Update - AuntomatedBoss
    Spawn point no longer require animation bind

--- AbyssWorks/Scripts/Abilities/IceProjectileAbility.gd
+++ AbyssWorks/Scripts/Abilities/IceProjectileAbility.gd
@@ -5,14 +5,19 @@ class_name IceProjectileAbility
 @export var attackDistance: float = 400
 @export var iceAttackAnims: Array[String] = []
 
+@export_group("Projectile control")
+@export var targetTrack: bool = false
+@export var projectileForce: float = 200
+
 @export_group("Damage")
 @export var damageAmount: float = 0
 @export var damageType: DamageInfo.DamageType = DamageInfo.DamageType.Ice
 
-var anim_spawn_dict: Dictionary[String, Node2D] = {}
+var spawn_dict: Dictionary[String, Node2D] = {}
 
 var characterBody2D: CharacterBody2D = null
 var anim_subsc: AnimationPlayer = null
+var target: Node2D = null
 
 var _cooldownTimer: float = 0
 var isExecuting: bool = false
@@ -20,12 +25,17 @@ var isExecuting: bool = false
 var _consecutiveCount: int = 1
 var _currentAnim: String = ""
 
-const ICE_PROJECTILE_ATTACK = preload("res://Graeme/Scenes/Attacks/default_projectile_attack.tscn")
+var spawnLocation: Node2D = null
+
+const ICE_PROJECTILE_ATTACK = preload("res://AbyssWorks/Prefabs/Projectiles/IceShootProjectile.tscn")
 
 func External_Ready() -> void:
 	characterBody2D = _variable_dict["char_body"]
 	anim_subsc = _variable_dict["anim_subsc"]
-	anim_spawn_dict = _variable_dict["anim_spawn_dict"]
+	spawn_dict = _variable_dict["spawn_dict"]
+	target = _variable_dict["target"]
+	
+	spawnLocation = spawn_dict["IceProjectileSP"]
 	
 	if anim_subsc:
 		anim_subsc.SubscribeCallable("Launch", self._spawn_on_current_anim)
@@ -96,19 +106,24 @@ func _condition_cooldown(delta):
 		
 func _spawn_on_current_anim():
 	if _currentAnim != "":
-		_spawn_projectile(anim_spawn_dict[_currentAnim])
+		_spawn_projectile(spawnLocation)
 
 func _spawn_projectile(spawnPoint: Node2D):
 	if not characterBody2D and not spawnPoint:
 		return
 	
-	var default_projectile_attack_instance = ICE_PROJECTILE_ATTACK.instantiate()
-	if default_projectile_attack_instance is DefaultProjectile:
-		var default_projectile: DefaultProjectile = default_projectile_attack_instance
-		default_projectile.instigator = characterBody2D
+	var shoot_projectile_attack_instance = ICE_PROJECTILE_ATTACK.instantiate()
+	if shoot_projectile_attack_instance is ShootProjectile:
+		var shoot_projectile: ShootProjectile = shoot_projectile_attack_instance
+		shoot_projectile.instigator = characterBody2D
+		shoot_projectile.damageInfo = DamageInfo.new(damageAmount, characterBody2D, damageType)
+		shoot_projectile.force = projectileForce
+		shoot_projectile.global_position = spawnPoint.global_position
+		if targetTrack and target:
+			shoot_projectile.shootDirection = (target.global_position - characterBody2D.global_position).normalized()
+		else:
+			shoot_projectile.shootDirection = characterBody2D.transform.x
 		
-	characterBody2D.get_tree().current_scene.add_child(default_projectile_attack_instance)
-	default_projectile_attack_instance.global_position = spawnPoint.global_position
+	characterBody2D.get_tree().current_scene.add_child(shoot_projectile_attack_instance)
 	
-	default_projectile_attack_instance.set_direction(sign(characterBody2D.transform.x.x))
 	pass
